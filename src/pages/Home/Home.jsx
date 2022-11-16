d3fa0f7ace211f5ae923b189e5d231083081544d
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-16 14:00:59 -0500

    space home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,11 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return ( 
-    <>
+    <div className={styles.landing}>
       <h1>Chris Steffen</h1>
       <h2>Software Engineer</h2>
       <img className={styles.image} src={Pic} alt="" />
-    </>
+    </div>
   );
 }
 
