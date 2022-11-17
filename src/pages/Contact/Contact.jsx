0713cd99e40cd644ae682c218ed47ff35d0fd39e
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-16 22:09:48 -0500

    small readablility changes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,9 +3,10 @@ const Contact = () => {
     <>
       <h1>Contact</h1>
       <div>
-        <p>Reach out if you'd liked to connect<a href="mailto:ChrisJSteffen@gmail.com ">email</a> <br />
+        <p>Reach out if you'd liked to connect <a href="mailto:ChrisJSteffen@gmail.com ">email</a> <br />
         Other links <br />
         <a href="https://www.linkedin.com/in/chris-steffen1/" alt="" target='_blank' rel="noreferrer">linkedin</a>
+        <br />
         <a href="https://github.com/chrsteffen1" alt='' target='_blank' rel="noreferrer">Github</a>
         </p>
       </div>
