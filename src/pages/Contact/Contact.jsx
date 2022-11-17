3dd5e7259382448320783e5c16a3e66e76bab377
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-17 14:10:09 -0500

    add email form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,27 @@
+import React from "react";
+import { useRef } from "react";
+import emailjs from 'emailjs-com'
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      process.env.REACT_APP_SERVICE_ID, 
+      process.env.REACT_APP_TEMPLATE_ID, 
+      form.current, 
+      process.env.REACT_APP_USER_ID
+      )
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      e.target.reset()
+  }
+
   return ( 
     <>
       <h1>Contact</h1>
@@ -9,6 +32,17 @@ const Contact = () => {
         <br />
         <a href="https://github.com/chrsteffen1" alt='' target='_blank' rel="noreferrer">Github</a>
         </p>
+      </div>
+      <div>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" required/>
+          <label>Email</label>
+          <input type="email" name="user_email" required/>
+          <label>Message</label>
+          <textarea name="message" required/>
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </>
   );
