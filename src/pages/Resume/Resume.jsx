05a89f6b24f2f02719530372a7bbe3c8fd3efb0a
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-17 13:42:08 -0500

    more styling

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,12 @@
 import resume from '../../assets/Chris_Steffen_Resume.pdf'
+import styles  from './Resume.module.css'
 
 const Resume = () => {
   return ( 
     <>
       <h1>Resume</h1>
-      <p>
-        <a href={resume} download>Resume</a>
+      <p> Click here to download my resume
+        <a href={resume} download> <button class={styles.buttonDownload}>Download</button></a>
       </p>
     </>
   );
