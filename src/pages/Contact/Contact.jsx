9f2c4558d131139d51e208b21aa557c8260a2061
Chrsteffen <Chrsteffen1@gmail.com>
2023-03-05 21:37:22 -0500

    update contact css

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,44 +1,39 @@
 import React from "react";
-import { useRef } from "react";
-import emailjs from 'emailjs-com'
 import styles from './Contact.module.css'
-import  gitIcon  from '../../assets/git_icon.png'
-import  linkedinIcon  from '../../assets/linkedin.png'
 import Button from 'react-bootstrap/Button';
 import resume from '../../assets/Chris_Steffen_Resume.pdf'
-import emailIcon from '../../assets/email_icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(
-      process.env.REACT_APP_SERVICE_ID, 
-      process.env.REACT_APP_TEMPLATE_ID, 
-      form.current, 
-      process.env.REACT_APP_USER_ID
-      )
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-      e.target.reset()
-  }
-
   return ( 
-    <>
-      
-      <div className={styles.formDiv}>
-        <a href={resume} download='Chris_Steffen_Resume'><Button variant="dark" className={styles.buttonDownload}>Download My Resume Here</Button></a>
-        <div className={styles.social}>
-          <a href="https://www.linkedin.com/in/chris-steffen1/" alt="" target='_blank' rel="noreferrer"><img src={linkedinIcon} alt="linkedin icon" className={styles.icons}/></a>
-          <a href="https://github.com/chrsteffen1" alt='' target='_blank' rel="noreferrer" ><img src={gitIcon} alt="git icon" className={styles.icons}/></a>
-          <a href="mailto:chrisJsteffen@gmailcom"><img src={emailIcon} alt="email icon" className={styles.icons}/></a>
-        </div>
-      </div>
+    <>  
+<section className={styles.contact}>
+  <h1 className={styles.title}>Contact</h1>
+  <div className={styles.buttonContainer}>
+    <Button variant="dark" className={styles.button} href={resume} download='Chris_Steffen_Resume'>
+    <FontAwesomeIcon icon={faFilePdf} className={styles.icon} />
+    Resume
+    </Button>
+    <Button variant="dark" className={styles.button} href="https://www.linkedin.com/in/chris-steffen1/">
+    <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+    LinkedIn
+    </Button>
+    <Button variant="dark" className={styles.button} href="https://github.com/chrsteffen1">
+    <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+    Github
+    </Button>
+    <Button variant="dark" className={styles.button} href="mailto:chrisJsteffen@gmailcom">
+    <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+    Email
+    </Button>
+  </div>
+</section>
+
     </>
   );
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -13,46 +13,26 @@ const Projects = (props) => {
   };
 
   return ( 
-    <section className={styles.carousel}>
-    <h1 className={styles.title}>Projects</h1>
-    <Carousel className={styles.container} activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className={styles.carImage}
-          src={projects[0].image}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className={styles.carImage}
-          src={projects[1].image}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className={styles.carImage}
-          src={projects[2].image}
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className={styles.carImage}
-          src={projects[3].image}
-          alt="Third slide"
-        />
-      </Carousel.Item> 
-    </Carousel>
-    <div className={styles.caption}>
-      <h3>{projects[index].title}</h3>
-    </div>
-    <div className={styles.caption}>
-      <Button variant="secondary"><a href={projects[index].repositoryLink}>Github</a></Button>
-      <Button variant="secondary"><a href={projects[index].deploymentLink}>Link</a></Button>
-      
-    </div>
+    <section className={styles.projects}>
+      <h1 className={styles.title}>Projects</h1>
+      <Carousel className={styles.container} activeIndex={index} onSelect={handleSelect}>
+        {projects.map((project, i) => (
+          <Carousel.Item key={i}>
+            <img
+              className={styles.image}
+              src={project.image}
+              alt={project.title}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <div className={styles.caption}>
+        <h3>{projects[index].title}</h3>
+      </div>
+      <div className={styles.caption}>
+        <Button variant="secondary"><a href={projects[index].repositoryLink}>Github</a></Button>
+        <Button variant="secondary"><a href={projects[index].deploymentLink}>Link</a></Button>
+      </div>
     </section>
   );
 }
