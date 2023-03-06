39fcc9b29fbf650a14f2edd8158d2d02965afa85
Chrsteffen <Chrsteffen1@gmail.com>
2023-03-06 10:27:56 -0500

    adjust css to smaller screens

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,28 +12,27 @@ import { faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 const Contact = () => {
   return ( 
     <>  
-<section className={styles.contact} id='contact'>
-  <h1 className={styles.title}>Contact</h1>
-  <div className={styles.buttonContainer}>
-    <Button variant="dark" className={styles.button} href={resume} download='Chris_Steffen_Resume'>
-    <FontAwesomeIcon icon={faFilePdf} className={styles.icon} />
-    Resume
-    </Button>
-    <Button variant="dark" className={styles.button} href="https://www.linkedin.com/in/chris-steffen1/">
-    <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
-    LinkedIn
-    </Button>
-    <Button variant="dark" className={styles.button} href="https://github.com/chrsteffen1">
-    <FontAwesomeIcon icon={faGithub} className={styles.icon} />
-    Github
-    </Button>
-    <Button variant="dark" className={styles.button} href="mailto:chrisJsteffen@gmailcom">
-    <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
-    Email
-    </Button>
-  </div>
-</section>
-
+      <section className={styles.contact} id='contact'>
+        <h1 className={styles.title}>Contact</h1>
+        <div className={styles.buttonContainer}>
+          <Button variant="dark" className={styles.button} href={resume} download='Chris_Steffen_Resume'>
+          <FontAwesomeIcon icon={faFilePdf} className={styles.icon} />
+          Resume
+          </Button>
+          <Button variant="dark" className={styles.button} href="https://www.linkedin.com/in/chris-steffen1/">
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+          LinkedIn
+          </Button>
+          <Button variant="dark" className={styles.button} href="https://github.com/chrsteffen1">
+          <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+          Github
+          </Button>
+          <Button variant="dark" className={styles.button} href="mailto:chrisJsteffen@gmailcom">
+          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+          Email
+          </Button>
+          </div>
+      </section>
     </>
   );
 }
