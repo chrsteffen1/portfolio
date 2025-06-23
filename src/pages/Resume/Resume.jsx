b3718872e5da35ee3cca0a79203215b8018d2dc1
chrsteffen1 <chrsteffen1@gmail.com>
2025-06-23 15:16:24 +0000

    removed pdf resume and added web link resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,10 @@
-import resume from '../../assets/Chris_Steffen_Resume.pdf'
 import styles  from './Resume.module.css'
 
 const Resume = () => {
-  return ( 
+  return (
     <section>
       <h1 id="resume">Resume</h1>
-      <p>Click here to download my resume<a href={resume} download='Chris_Steffen_Resume'><button className={styles.buttonDownload}>Download</button></a>
+      <p>Click here to view my resume<a href="https://docs.google.com/document/d/135vuVJoC3ErVarGzMLxBiob3KqC-vZvvZpu8e7oYK0A/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><button className={styles.buttonDownload}>View Resume</button></a>
       </p>
     </section>
   );
